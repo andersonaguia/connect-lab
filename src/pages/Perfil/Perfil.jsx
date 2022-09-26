4bdfd9c6b9b229b295e696120b7050b3bc05bba3
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-09-25 23:46:45 -0300

    foto

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -25,7 +25,7 @@ export const Perfil = () => {
     return(
         <SectionStyled>
                 <H2Styled>Meu perfil</H2Styled>            
-                <ImgStyled src={isAuthenticated.user.photoUrl ? isAuthenticated.user.photoUrl : '../../../public/avatar.png'} alt="foto do perfil"></ImgStyled>              
+                <ImgStyled src={isAuthenticated.user.photoUrl ? isAuthenticated.user.photoUrl : '/avatar.png'} alt="foto do perfil"></ImgStyled>              
                 <H3Styled>{isAuthenticated.user.fullName}</H3Styled>                                       
                 <a href="/">{isAuthenticated.user.email}</a>
                 <p>
